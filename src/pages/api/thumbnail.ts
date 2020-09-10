f49150bf7299fe829b583bc81e31e4bf521de1e4
Anton Lin <antonjlin@gmail.com>
2020-09-10 12:18:30 -0700

    klsdjfl

--- src/pages/api/thumbnail.ts
+++ src/pages/api/thumbnail.ts
@@ -1,26 +1,28 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import * as playwright from 'playwright-aws-lambda';
-import { getAbsoluteURL } from 'utils/utils';
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as playwright from "playwright-aws-lambda";
+import { getAbsoluteURL } from "utils/utils";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await playwright.loadFont("https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf")
+  await playwright.loadFont(
+    "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+  );
 
   const browser = await playwright.launchChromium();
   const page = await browser.newPage({
     viewport: {
       width: 1200,
-      height: 630
-    }
+      height: 630,
+    },
   });
-  const url = getAbsoluteURL(req.query["path"] as string || "")
+  const url = (req.query["path"] as string) || "";
   await page.goto(url, {
-    timeout: 15 * 1000
-  })
+    timeout: 15 * 1000,
+  });
   const data = await page.screenshot({
-    type: "png"
-  })
-  await browser.close()
-  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
-  res.setHeader('Content-Type', 'image/png')
-  res.end(data)
-}
+    type: "png",
+  });
+  await browser.close();
+  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
+  res.setHeader("Content-Type", "image/png");
+  res.end(data);
+};
